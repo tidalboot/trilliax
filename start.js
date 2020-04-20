7e83f9f8e8e107b27dba7dc6c36715997229ca7c
Nick Jones <tidalboot@googlemail.com>
2020-04-20 16:14:07 +0100

    complete change to the function of trill in that he is now a quiz bot

--- start.js
+++ start.js
@@ -1,58 +1,267 @@
 var allPokemon = ["BULBASAUR","IVYSAUR","VENUSAUR","CHARMANDER","CHARMELEON","CHARIZARD","SQUIRTLE","WARTORTLE","BLASTOISE","CATERPIE","METAPOD","BUTTERFREE","WEEDLE","KAKUNA","BEEDRILL","PIDGEY","PIDGEOTTO","PIDGEOT","RATTATA","RATICATE","SPEAROW","FEAROW","EKANS","ARBOK","PIKACHU","RAICHU","SANDSHREW","SANDSLASH","NIDORAN♀","NIDORINA","NIDOQUEEN","NIDORAN♂","NIDORINO","NIDOKING","CLEFAIRY","CLEFABLE","VULPIX","NINETALES","JIGGLYPUFF","WIGGLYTUFF","ZUBAT","GOLBAT","ODDISH","GLOOM","VILEPLUME","PARAS","PARASECT","VENONAT","VENOMOTH","DIGLETT","DUGTRIO","MEOWTH","PERSIAN","PSYDUCK","GOLDUCK","MANKEY","PRIMEAPE","GROWLITHE","ARCANINE","POLIWAG","POLIWHIRL","POLIWRATH","ABRA","KADABRA","ALAKAZAM","MACHOP","MACHOKE","MACHAMP","BELLSPROUT","WEEPINBELL","VICTREEBEL","TENTACOOL","TENTACRUEL","GEODUDE","GRAVELER","GOLEM","PONYTA","RAPIDASH","SLOWPOKE","SLOWBRO","MAGNEMITE","MAGNETON","FARFETCH’D","DODUO","DODRIO","SEEL","DEWGONG","GRIMER","MUK","SHELLDER","CLOYSTER","GASTLY","HAUNTER","GENGAR","ONIX","DROWZEE","HYPNO","KRABBY","KINGLER","VOLTORB","ELECTRODE","EXEGGCUTE","EXEGGUTOR","CUBONE","MAROWAK","HITMONLEE","HITMONCHAN","LICKITUNG","KOFFING","WEEZING","RHYHORN","RHYDON","CHANSEY","TANGELA","KANGASKHAN","HORSEA","SEADRA","GOLDEEN","SEAKING","STARYU","STARMIE","MR. MIME","SCYTHER","JYNX","ELECTABUZZ","MAGMAR","PINSIR","TAUROS","MAGIKARP","GYARADOS","LAPRAS","DITTO","EEVEE","VAPOREON","JOLTEON","FLAREON","PORYGON","OMANYTE","OMASTAR","KABUTO","KABUTOPS","AERODACTYL","SNORLAX","ARTICUNO","ZAPDOS","MOLTRES","DRATINI","DRAGONAIR","DRAGONITE","MEWTWO","MEW","CHIKORITA","BAYLEEF","MEGANIUM","CYNDAQUIL","QUILAVA","TYPHLOSION","TOTODILE","CROCONAW","FERALIGATR","SENTRET","FURRET","HOOTHOOT","NOCTOWL","LEDYBA","LEDIAN","SPINARAK","ARIADOS","CROBAT","CHINCHOU","LANTURN","PICHU","CLEFFA","IGGLYBUFF","TOGEPI","TOGETIC","NATU","XATU","MAREEP","FLAAFFY","AMPHAROS","BELLOSSOM","MARILL","AZUMARILL","SUDOWOODO","POLITOED","HOPPIP","SKIPLOOM","JUMPLUFF","AIPOM","SUNKERN","SUNFLORA","YANMA","WOOPER","QUAGSIRE","ESPEON","UMBREON","MURKROW","SLOWKING","MISDREAVUS","UNOWN","WOBBUFFET","GIRAFARIG","PINECO","FORRETRESS","DUNSPARCE","GLIGAR","STEELIX","SNUBBULL","GRANBULL","QWILFISH","SCIZOR","SHUCKLE","HERACROSS","SNEASEL","TEDDIURSA","URSARING","SLUGMA","MAGCARGO","SWINUB","PILOSWINE","CORSOLA","REMORAID","OCTILLERY","DELIBIRD","MANTINE","SKARMORY","HOUNDOUR","HOUNDOOM","KINGDRA","PHANPY","DONPHAN","PORYGON2","STANTLER","SMEARGLE","TYROGUE","HITMONTOP","SMOOCHUM","ELEKID","MAGBY","MILTANK","BLISSEY","RAIKOU","ENTEI","SUICUNE","LARVITAR","PUPITAR","TYRANITAR","LUGIA","HO-OH","CELEBI","TREECKO","GROVYLE","SCEPTILE","TORCHIC","COMBUSKEN","BLAZIKEN","MUDKIP","MARSHTOMP","SWAMPERT","POOCHYENA","MIGHTYENA","ZIGZAGOON","LINOONE","WURMPLE","SILCOON","BEAUTIFLY","CASCOON","DUSTOX","LOTAD","LOMBRE","LUDICOLO","SEEDOT","NUZLEAF","SHIFTRY","TAILLOW","SWELLOW","WINGULL","PELIPPER","RALTS","KIRLIA","GARDEVOIR","SURSKIT","MASQUERAIN","SHROOMISH","BRELOOM","SLAKOTH","VIGOROTH","SLAKING","NINCADA","NINJASK","SHEDINJA","WHISMUR","LOUDRED","EXPLOUD","MAKUHITA","HARIYAMA","AZURILL","NOSEPASS","SKITTY","DELCATTY","SABLEYE","MAWILE","ARON","LAIRON","AGGRON","MEDITITE","MEDICHAM","ELECTRIKE","MANECTRIC","PLUSLE","MINUN","VOLBEAT","ILLUMISE","ROSELIA","GULPIN","SWALOT","CARVANHA","SHARPEDO","WAILMER","WAILORD","NUMEL","CAMERUPT","TORKOAL","SPOINK","GRUMPIG","SPINDA","TRAPINCH","VIBRAVA","FLYGON","CACNEA","CACTURNE","SWABLU","ALTARIA","ZANGOOSE","SEVIPER","LUNATONE","SOLROCK","BARBOACH","WHISCASH","CORPHISH","CRAWDAUNT","BALTOY","CLAYDOL","LILEEP","CRADILY","ANORITH","ARMALDO","FEEBAS","MILOTIC","CASTFORM","KECLEON","SHUPPET","BANETTE","DUSKULL","DUSCLOPS","TROPIUS","CHIMECHO","ABSOL","WYNAUT","SNORUNT","GLALIE","SPHEAL","SEALEO","WALREIN","CLAMPERL","HUNTAIL","GOREBYSS","RELICANTH","LUVDISC","BAGON","SHELGON","SALAMENCE","BELDUM","METANG","METAGROSS","REGIROCK","REGICE","REGISTEEL","LATIAS","LATIOS","KYOGRE","GROUDON","RAYQUAZA","JIRACHI","DEOXYS","TURTWIG","GROTLE","TORTERRA","CHIMCHAR","MONFERNO","INFERNAPE","PIPLUP","PRINPLUP","EMPOLEON","STARLY","STARAVIA","STARAPTOR","BIDOOF","BIBAREL","KRICKETOT","KRICKETUNE","SHINX","LUXIO","LUXRAY","BUDEW","ROSERADE","CRANIDOS","RAMPARDOS","SHIELDON","BASTIODON","BURMY","WORMADAM","MOTHIM","COMBEE","VESPIQUEN","PACHIRISU","BUIZEL","FLOATZEL","CHERUBI","CHERRIM","SHELLOS","GASTRODON","AMBIPOM","DRIFLOON","DRIFBLIM","BUNEARY","LOPUNNY","MISMAGIUS","HONCHKROW","GLAMEOW","PURUGLY","CHINGLING","STUNKY","SKUNTANK","BRONZOR","BRONZONG","BONSLY","MIME JR.","HAPPINY","CHATOT","SPIRITOMB","GIBLE","GABITE","GARCHOMP","MUNCHLAX","RIOLU","LUCARIO","HIPPOPOTAS","HIPPOWDON","SKORUPI","DRAPION","CROAGUNK","TOXICROAK","CARNIVINE","FINNEON","LUMINEON","MANTYKE","SNOVER","ABOMASNOW","WEAVILE","MAGNEZONE","LICKILICKY","RHYPERIOR","TANGROWTH","ELECTIVIRE","MAGMORTAR","TOGEKISS","YANMEGA","LEAFEON","GLACEON","GLISCOR","MAMOSWINE","PORYGON-Z","GALLADE","PROBOPASS","DUSKNOIR","FROSLASS","ROTOM","UXIE","MESPRIT","AZELF","DIALGA","PALKIA","HEATRAN","REGIGIGAS","GIRATINA","CRESSELIA","PHIONE","MANAPHY","DARKRAI","SHAYMIN","ARCEUS","VICTINI","SNIVY","SERVINE","SERPERIOR","TEPIG","PIGNITE","EMBOAR","OSHAWOTT","DEWOTT","SAMUROTT","PATRAT","WATCHOG","LILLIPUP","HERDIER","STOUTLAND","PURRLOIN","LIEPARD","PANSAGE","SIMISAGE","PANSEAR","SIMISEAR","PANPOUR","SIMIPOUR","MUNNA","MUSHARNA","PIDOVE","TRANQUILL","UNFEZANT","BLITZLE","ZEBSTRIKA","ROGGENROLA","BOLDORE","GIGALITH","WOOBAT","SWOOBAT","DRILBUR","EXCADRILL","AUDINO","TIMBURR","GURDURR","CONKELDURR","TYMPOLE","PALPITOAD","SEISMITOAD","THROH","SAWK","SEWADDLE","SWADLOON","LEAVANNY","VENIPEDE","WHIRLIPEDE","SCOLIPEDE","COTTONEE","WHIMSICOTT","PETILIL","LILLIGANT","BASCULIN","SANDILE","KROKOROK","KROOKODILE","DARUMAKA","DARMANITAN","MARACTUS","DWEBBLE","CRUSTLE","SCRAGGY","SCRAFTY","SIGILYPH","YAMASK","COFAGRIGUS","TIRTOUGA","CARRACOSTA","ARCHEN","ARCHEOPS","TRUBBISH","GARBODOR","ZORUA","ZOROARK","MINCCINO","CINCCINO","GOTHITA","GOTHORITA","GOTHITELLE","SOLOSIS","DUOSION","REUNICLUS","DUCKLETT","SWANNA","VANILLITE","VANILLISH","VANILLUXE","DEERLING","SAWSBUCK","EMOLGA","KARRABLAST","ESCAVALIER","FOONGUS","AMOONGUSS","FRILLISH","JELLICENT","ALOMOMOLA","JOLTIK","GALVANTULA","FERROSEED","FERROTHORN","KLINK","KLANG","KLINKLANG","TYNAMO","EELEKTRIK","EELEKTROSS","ELGYEM","BEHEEYEM","LITWICK","LAMPENT","CHANDELURE","AXEW","FRAXURE","HAXORUS","CUBCHOO","BEARTIC","CRYOGONAL","SHELMET","ACCELGOR","STUNFISK","MIENFOO","MIENSHAO","DRUDDIGON","GOLETT","GOLURK","PAWNIARD","BISHARP","BOUFFALANT","RUFFLET","BRAVIARY","VULLABY","MANDIBUZZ","HEATMOR","DURANT","DEINO","ZWEILOUS","HYDREIGON","LARVESTA","VOLCARONA","COBALION","TERRAKION","VIRIZION","TORNADUS","THUNDURUS","RESHIRAM","ZEKROM","LANDORUS","KYUREM","KELDEO","MELOETTA","GENESECT","CHESPIN","QUILLADIN","CHESNAUGHT","FENNEKIN","BRAIXEN","DELPHOX","FROAKIE","FROGADIER","GRENINJA","BUNNELBY","DIGGERSBY","FLETCHLING","FLETCHINDER","TALONFLAME","SCATTERBUG","SPEWPA","VIVILLON","LITLEO","PYROAR","FLABÉBÉ","FLOETTE","FLORGES","SKIDDO","GOGOAT","PANCHAM","PANGORO","FURFROU","ESPURR","MEOWSTIC","HONEDGE","DOUBLADE","AEGISLASH","SPRITZEE","AROMATISSE","SWIRLIX","SLURPUFF","INKAY","MALAMAR","BINACLE","BARBARACLE","SKRELP","DRAGALGE","CLAUNCHER","CLAWITZER","HELIOPTILE","HELIOLISK","TYRUNT","TYRANTRUM","AMAURA","AURORUS","SYLVEON","HAWLUCHA","DEDENNE","CARBINK","GOOMY","SLIGGOO","GOODRA","KLEFKI","PHANTUMP","TREVENANT","PUMPKABOO","GOURGEIST","BERGMITE","AVALUGG","NOIBAT","NOIVERN","XERNEAS","YVELTAL","ZYGARDE","DIANCIE","HOOPA","VOLCANION","ROWLET","DARTRIX","DECIDUEYE","LITTEN","TORRACAT","INCINEROAR","POPPLIO","BRIONNE","PRIMARINA","PIKIPEK","TRUMBEAK","TOUCANNON","YUNGOOS","GUMSHOOS","GRUBBIN","CHARJABUG","VIKAVOLT","CRABRAWLER","CRABOMINABLE","ORICORIO","CUTIEFLY","RIBOMBEE","ROCKRUFF","LYCANROC","WISHIWASHI","MAREANIE","TOXAPEX","MUDBRAY","MUDSDALE","DEWPIDER","ARAQUANID","FOMANTIS","LURANTIS","MORELULL","SHIINOTIC","SALANDIT","SALAZZLE","STUFFUL","BEWEAR","BOUNSWEET","STEENEE","TSAREENA","COMFEY","ORANGURU","PASSIMIAN","WIMPOD","GOLISOPOD","SANDYGAST","PALOSSAND","PYUKUMUKU","TYPE: NULL","SILVALLY","MINIOR","KOMALA","TURTONATOR","TOGEDEMARU","MIMIKYU","BRUXISH","DRAMPA","DHELMISE","JANGMO-O","HAKAMO-O","KOMMO-O","TAPU KOKO","TAPU LELE","TAPU BULU","TAPU FINI","COSMOG","COSMOEM","SOLGALEO","LUNALA","NIHILEGO","BUZZWOLE","PHEROMOSA","XURKITREE","CELESTEELA","KARTANA","GUZZLORD","NECROZMA","MAGEARNA","MARSHADOW","POIPOLE","NAGANADEL","STAKATAKA","BLACEPHALON","ZERAORA","GROOKEY","THWACKEY","","RILLABOOM","SCORBUNNY","RABOOT","CINDERACE","SOBBLE","DRIZZILE","INTELEON","SKWOVET","GREEDENT","ROOKIDEE","CORVISQUIRE","CORVIKNIGHT","BLIPBUG","DOTTLER","ORBEETLE","NICKIT","THIEVUL","GOSSIFLEUR","ELDEGOSS","WOOLOO","DUBWOOL","CHEWTLE","DREDNAW","YAMPER","BOLTUND","ROLYCOLY","CARKOL","COALOSSAL","APPLIN","FLAPPLE","APPLETUN","SILICOBRA","SANDACONDA","CRAMORANT","ARROKUDA","BARRASKEWDA","TOXEL","TOXTRICITY","SIZZLIPEDE","CENTISKORCH","CLOBBOPUS","GRAPPLOCT","SINISTEA","POLTEAGEIST","HATENNA","HATTREM","HATTERENE","IMPIDIMP","MORGREM","GRIMMSNARL","OBSTAGOON","PERRSERKER","CURSOLA","SIRFETCH'D","MR. RIME","RUNERIGUS","MILCERY","ALCREMIE","FALINKS","PINCURCHIN","SNOM","FROSMOTH","STONJOURNER","EISCUE","INDEEDEE","MORPEKO","CUFANT","COPPERAJAH","DRACOZOLT","ARCTOZOLT","DRACOVISH","ARCTOVISH","DURALUDON","DREEPY","DRAKLOAK","DRAGAPULT","ZACIAN","ZAMAZENTA","ETERNATUS","KUBFU","URSHIFU","ZARUDE","CALYREX"]
+var originalPokemon = ["BULBASAUR","IVYSAUR","VENUSAUR","CHARMANDER","CHARMELEON","CHARIZARD","SQUIRTLE","WARTORTLE","BLASTOISE","CATERPIE","METAPOD","BUTTERFREE","WEEDLE","KAKUNA","BEEDRILL","PIDGEY","PIDGEOTTO","PIDGEOT","RATTATA","RATICATE","SPEAROW","FEAROW","EKANS","ARBOK","PIKACHU","RAICHU","SANDSHREW","SANDSLASH","NIDORAN♀","NIDORINA","NIDOQUEEN","NIDORAN♂","NIDORINO","NIDOKING","CLEFAIRY","CLEFABLE","VULPIX","NINETALES","JIGGLYPUFF","WIGGLYTUFF","ZUBAT","GOLBAT","ODDISH","GLOOM","VILEPLUME","PARAS","PARASECT","VENONAT","VENOMOTH","DIGLETT","DUGTRIO","MEOWTH","PERSIAN","PSYDUCK","GOLDUCK","MANKEY","PRIMEAPE","GROWLITHE","ARCANINE","POLIWAG","POLIWHIRL","POLIWRATH","ABRA","KADABRA","ALAKAZAM","MACHOP","MACHOKE","MACHAMP","BELLSPROUT","WEEPINBELL","VICTREEBEL","TENTACOOL","TENTACRUEL","GEODUDE","GRAVELER","GOLEM","PONYTA","RAPIDASH","SLOWPOKE","SLOWBRO","MAGNEMITE","MAGNETON","FARFETCH’D","DODUO","DODRIO","SEEL","DEWGONG","GRIMER","MUK","SHELLDER","CLOYSTER","GASTLY","HAUNTER","GENGAR","ONIX","DROWZEE","HYPNO","KRABBY","KINGLER","VOLTORB","ELECTRODE","EXEGGCUTE","EXEGGUTOR","CUBONE","MAROWAK","HITMONLEE","HITMONCHAN","LICKITUNG","KOFFING","WEEZING","RHYHORN","RHYDON","CHANSEY","TANGELA","KANGASKHAN","HORSEA","SEADRA","GOLDEEN","SEAKING","STARYU","STARMIE","MR. MIME","SCYTHER","JYNX","ELECTABUZZ","MAGMAR","PINSIR","TAUROS","MAGIKARP","GYARADOS","LAPRAS","DITTO","EEVEE","VAPOREON","JOLTEON","FLAREON","PORYGON","OMANYTE","OMASTAR","KABUTO","KABUTOPS","AERODACTYL","SNORLAX","ARTICUNO","ZAPDOS","MOLTRES","DRATINI","DRAGONAIR","DRAGONITE","MEWTWO","MEW"]
 
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
-// Configure logger settings
+var fs = require('fs');
+
+var master = "177138739787595776";
+var trilliax = "700704160437370921";
+
+let testingChannel = "700717192072921088"
+let generalChannel = "620667360994394123"
+var channelToUse = generalChannel
+
+var minimumSpawnTime = 6000
+var maximumSpawnTime = minimumSpawnTime * 6
+
+var pokemonSpawned
+var currentQuizPlayers
+var pokemonAvailableToGuess = false
+
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
     colorize: true
 });
 logger.level = 'debug';
-// Initialize Discord Bot
+
 var bot = new Discord.Client({
    token: auth.token,
    autorun: true
 });
+
 bot.on('ready', function (evt) {
+
     logger.info('Connected');
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
+    
+    if (fs.existsSync('../trainers.json') == false) { createTrainerFile(); return }
+
+    setUpSpawnTimer()
+
+    var rawTrainerData = fs.readFileSync('../trainers.json');
+    currentQuizPlayers = JSON.parse(rawTrainerData);
+
+    console.log(currentQuizPlayers)
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
 
-    if (userID == "365975655608745985") { return }
-    if ((channelID == "698874728932180018" || channelID == "700717192072921088") == false) { return }
-    if (user == "Trilliax") { return }
-    if (message == "p!hint") { return }
+    if (userID == trilliax) { return }
+    if (userID == master && message == "who's that pokemon?") { spawnRandomPokemon(channelID); return }
+    if (userID == master && message == "you're tracking mud everywhere") { switchToTestingChannel(); return }
+    if (userID == master && message == "i know it's your favourite") { switchToGeneralChannel(); return }
 
     console.log("Message from: " + user + " - " + message)
 
-    var messageComponenets = message.split(' ')
-    if (messageComponenets.length != 2) {
-        bot.sendMessage({
-            to: channelID,
-            message: 'Filthy!'
-        });
-        bot.deleteMessage({
-            channelID: channelID,
-            messageID: evt.d.id
-        });
-        return;
-    }
-    var commandPassed = message.split(' ')[0]
-    var pokemonSpecified = message.split(' ')[1].toUpperCase()
+    var messageComponents = message.split(' ')
 
-    if (allPokemon.includes(pokemonSpecified) && (commandPassed == "p!catch" || commandPassed == "p!hint")) { 
+    if (messageComponents.length < 1) { return }
+    var commandPassed = messageComponents[0]
+
+    if (commandPassed == "pokejoin") { addUserToQuizList(userID, user, channelID); return }
+    if (commandPassed == "pokescore") { getUserScore(userID, channelID); return }
+    if (commandPassed == "pokeboard") { getLeaderboard(channelID); return }
+    if (commandPassed == "spawntimer" && userID == master) { changeSpawnTimerTo(messageComponents[1]); return }
+
+    if (messageComponents.length < 2) { return }
+    var pokemonGuessed = messageComponents[1].toUpperCase()
+
+    if (commandPassed == "pokeguess") { handlePokemonGuess(channelID, userID, pokemonGuessed); return }
+
+});
+
+function changeSpawnTimerTo(newSpawntimer) {
+    minimumSpawnTime = newSpawntimer
+}
+
+function switchToTestingChannel() {
+    channelToUse = testingChannel
+}
+
+function switchToGeneralChannel() {
+    channelToUse = generalChannel
+}
+
+function getLeaderboard(channelID) {
+    
+    var allPlayers = Object.keys(currentQuizPlayers)
+    console.log(allPlayers)
+
+    var players = new Array();
+
+    for (var player in currentQuizPlayers) {
+        players.push({"trainerName" : currentQuizPlayers[player].trainerName, "score": currentQuizPlayers[player].score})
+    }
+
+    function leaderboardSort(a, b) {
+        if (a.score < b.score) return 1;
+        if (b.score < a.score) return -1;
+      
+        return 0;
+    }
+      
+    players.sort(leaderboardSort)
+
+    var playerScores = players.map(function(player, index) {
+        return (index + 1) + ": " + player.trainerName + " - " + player.score;
+    });
+    console.log(playerScores)
+
+    bot.sendMessage({
+        to: channelID,
+        message: playerScores.join("\n")
+    });
+}
+
+function setUpSpawnTimer() {
+
+    var nextSpawnTime = (Math.floor(Math.random() * Math.floor(minimumSpawnTime)) + maximumSpawnTime) * 1000
+
+    console.log("Next pokemon will spawn in " + nextSpawnTime / 1000 + " seconds")
+    
+    setTimeout(spawnRandomPokemon, nextSpawnTime, channelToUse)
+}
+
+function getUserScore(userID, channelID) {
+
+    if (userExists(userID) == false) { return }
+
+    bot.sendMessage({
+        to: channelID,
+        message: "<@" + userID + "> your current score is " + currentQuizPlayers[userID].score
+    });
+}
+
+function setUpGuessTimer() {
+
+    setTimeout(pokemonGuessTimerWarning, 30000, channelToUse)
+    setTimeout(pokemonNotGuessedInTime, 60000, channelToUse)
+}
+
+function userExists(userID) {
+    return currentQuizPlayers[userID] != null
+}
+
+function pokemonGuessTimerWarning(channelID) {
+
+    if (pokemonAvailableToGuess == false) { return }
+
+    bot.sendMessage({
+        to: channelID,
+        message: "30 seconds left to guess"
+    });
+}
+
+function pokemonNotGuessedInTime(channelID) {
+
+    if (pokemonAvailableToGuess == false) { return }
+
+    bot.sendMessage({
+        to: channelID,
+        message: "It got away!"
+    });
+
+    setUpSpawnTimer();
+    pokemonAvailableToGuess = false
+}
+
+function handlePokemonGuess(channelID, userID, pokemonGuessed) {
+
+    if (pokemonSpawned == null) { return }
+
+    if (!userExists(userID)) { 
         bot.sendMessage({
             to: channelID,
-            message: 'Legit Pokemon'
+            message: "<@" + userID + "> please join the quiz list using 'pokejoin'"
         });
         return 
     }
 
-    bot.deleteMessage({
-        channelID: channelID,
-        messageID: evt.d.id
+    var player = currentQuizPlayers[userID]
+    console.log(userID + " guessed " + pokemonGuessed)
+    console.log("current pokemon is " + pokemonSpawned)
+
+    if (pokemonGuessed != pokemonSpawned) {
+        bot.sendMessage({ to: channelID, message: "<@" + userID +"> incorrect <:afl:693967159608737843>" });
+        return
+    }
+    
+    pokemonAvailableToGuess = false
+    player.score += 1
+    pokemonSpawned = null
+
+    updatePlayerScore(userID, player.score)
+    setUpSpawnTimer()
+
+    bot.sendMessage({ to: channelID, message: "correct <@" + userID + "> your score is " + player.score });
+}
+
+function updatePlayerScore(userID, newScore) {
+
+    if (currentQuizPlayers == null) { return }
+    if (currentQuizPlayers[userID] == null) { return }
+
+    currentQuizPlayers[userID].score = newScore
+
+    let stringifiedTrainers = JSON.stringify(currentQuizPlayers, null, 2);
+
+    console.log("Writing new score for - " + userID)
+
+    fs.writeFile('../trainers.json', stringifiedTrainers, (err) => {
+         console.log('Data written to file');
     });
-});
+}
+
+function spawnRandomPokemon(channelID) {
+
+    pokemonAvailableToGuess = true
+    
+    var randomPokemonID = Math.floor(Math.random() * Math.floor(originalPokemon.length - 1))
+    pokemonSpawned = originalPokemon[randomPokemonID - 1];
+
+    setUpGuessTimer()
+    bot.sendMessage({
+        to: channelID,
+        message: "https://pokeres.bastionbot.org/images/pokemon/" + randomPokemonID + ".png"
+    });
+}
+
+function addUserToQuizList(userID, user, channelID) {
+
+    console.log("Attempting to add trainer - " + userID + " to trainer list")
+
+    if (currentQuizPlayers == null) { return }
+    if (currentQuizPlayers[userID] != null) { return }
+
+    currentQuizPlayers[userID] = 
+    { 
+        "trainerName": user, 
+        "score": 0
+    }
+
+    let stringifiedTrainers = JSON.stringify(currentQuizPlayers, null, 2);
+
+    fs.writeFile('../trainers.json', stringifiedTrainers, (err) => {
+        console.log('Data written to file');
+    });
+
+    bot.sendMessage({
+        to: channelID,
+        message: "Added to the quiz list " + " <@" + userID + ">" 
+    });
+}
+
+function createTrainerFile() {
+    
+    let trainer = { };
+     
+    let data = JSON.stringify(trainer, null, 2);
+    
+    fs.writeFile('../trainers.json', data, (err) => {
+        console.log('Data written to file');
+    });
+
+    currentQuizPlayers = {};
+}
